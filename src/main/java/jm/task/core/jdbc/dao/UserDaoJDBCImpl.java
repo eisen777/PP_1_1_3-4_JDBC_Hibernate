1a11e5469baefc07fb04a76bd8192d11b65db1ac
eisen777 <60812951+eisen777@users.noreply.github.com>
2023-09-29 20:49:54 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,122 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS users(" +
+                "ID BIGINT NOT NULL AUTO_INCREMENT, NAME VARCHAR(100), " +
+                "LASTNAME VARCHAR(100), AGE INT, PRIMARY KEY (ID) )";
 
+        try (PreparedStatement preparedStatement = Util.getMySQLConnection()
+                .prepareStatement(sql)) {
+
+            preparedStatement.executeUpdate(sql);
+            System.out.println("Table was created!");
+
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
+        String sql = "DROP TABLE IF EXISTS users";
 
+        try (PreparedStatement preparedStatement = Util.getMySQLConnection()
+                .prepareStatement(sql)) {
+
+            preparedStatement.executeUpdate(sql);
+            System.out.println("Table was dropped");
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String SQL = "INSERT INTO users.users (name, lastName, age) VALUES (?, ?, ?)";
+        try(PreparedStatement preparedStatement = Util.getMySQLConnection()
+                .prepareStatement(SQL)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+            System.out.println("User was added!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void removeUserById(long id) {
+        String sql = "DELETE FROM users WHERE ID=?";
 
+        try (PreparedStatement preparedStatement = Util.getMySQLConnection()
+                .prepareStatement(sql)) {
+
+            preparedStatement.setLong(1, id);
+
+            preparedStatement.executeUpdate();
+            System.out.println("User was removed!");
+
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        Statement statement;
+        String sql = "select * from users.users";
+
+        try(PreparedStatement preparedStatement = Util.getMySQLConnection()
+                .prepareStatement(sql)) {
+            //statement = Util.getMySQLConnection().createStatement();
+
+            ResultSet resultSet = preparedStatement.executeQuery(sql);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return users;
     }
 
-    public void cleanUsersTable() {
 
+    public void cleanUsersTable() {
+        String sql = "DELETE FROM users";
+        try (PreparedStatement preparedStatement = Util.getMySQLConnection()
+                .prepareStatement(sql)) {
+
+            preparedStatement.executeUpdate(sql);
+            System.out.println("Table was cleaned!");
+
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
